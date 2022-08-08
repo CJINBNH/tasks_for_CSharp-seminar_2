31c54d76f58d0b4b5783c34d53feb8452f8c9564
CJINBNH <alex_egorov_89@mail.ru>
2022-08-08 22:08:01 +0400

    Completed task003

---
+++ task003/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое второе: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+if (numberOne%numberTwo == 0)
+{
+    Console.WriteLine("Является кратным");
+}
+else
+{
+    Console.WriteLine(numberOne%numberTwo);
+}
