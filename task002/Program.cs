4bf61e6e1e05c8636eb8a688e9269a164d2d0d75
CJINBNH <alex_egorov_89@mail.ru>
2022-08-08 21:37:47 +0400

    Completed task002

---
+++ task002/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+int number = new Random().Next(100,1000);
+Console.WriteLine ($"Наше случайное число: {number}");
+int firstFigure = number/100;
+int thirdFigure = number%10;
+Console.Write(firstFigure*10+thirdFigure);
+
