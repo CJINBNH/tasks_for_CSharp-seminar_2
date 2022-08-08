f0d7a51c0ec43d62c641c5656a0d52afd7b9202f
CJINBNH <alex_egorov_89@mail.ru>
2022-08-08 22:43:22 +0400

    Completed task005

---
+++ task005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое второе: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+if (numberOne == numberTwo*numberTwo || numberTwo == numberOne*numberOne)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
