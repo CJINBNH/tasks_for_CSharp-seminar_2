5a6344e19e61552acb1a4845793b741eba75b009
CJINBNH <alex_egorov_89@mail.ru>
2022-08-08 21:41:47 +0400

    Completed task001

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Наше случайное число: {number}");
+int firstFigure = number / 10;
+int secondFigure = number % 10;
+if (firstFigure > secondFigure)
+{
+    Console.Write(firstFigure);
+}
+else
+{
+    Console.Write(secondFigure);
+}
+
